08d74633df880ff809b295f8451fcb4a5dd7ed48
zzz <1808814901@qq.com>
2023-10-10 19:07:36 +0800

    修改目录

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,7 @@
 <script setup></script>
 
 <template>
-  <header></header>
-
-  <RouterView />
+  <div>我是APP</div>
 </template>
 
-<style scoped>
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
+<style scoped></style>
